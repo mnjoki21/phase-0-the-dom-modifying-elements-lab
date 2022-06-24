e78a691f436b3a8d4950a4c7692b8ad03d6331a4
mitchelle <mitchellenjoki7@gmail.com>
2022-06-24 12:53:40 +0300

    Done.

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ document.querySelector("main#main").remove();
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
 newHeader.innerHTML = "Mitchelle is the champion!";
-newHeader.className = "victory";
+newHeader.className = "victory"; 
+
+
